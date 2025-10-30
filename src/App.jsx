6a45cfadc92c2a8a02361b9361172adbce0995b6
Narayan Bt <narayan.pre@gmail.com>
2025-10-29 18:53:50 -0600

    Copiado de imagenes y archivos de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -8,26 +8,51 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header className="header">
+  <img
+    src="./images/Logo_white.png"
+    alt="Logo del proyecto Around US"
+    className="header__logo"
+  />
+</header>
+
+<section className="profile">
+  <div className="profile__image-wrapper">
+    <img
+      src="./images/Profile_pic.png"
+      alt="Imagen de perfil"
+      className="profile__image"
+    />
+    <img
+      src="./images/editProfileButton.png"
+      alt="Botón de editar perfil"
+      className="profile__editButton"
+    />
+  </div>
+
+  <div className="profile__info">
+    <h1 className="profile__name">Jacques Cousteau</h1>
+    <p className="profile__role">Explorador</p>
+    <button className="profile__edit-button">
+      <img
+        src="./images/Edit_Button.png"
+        alt="botón para editar"
+        className="profile__edit-image"
+      />
+    </button>
+  </div>
+
+  <button className="profile__add-button">
+    <img src="./images/Add_Button.png" alt="botón para editar" />
+  </button>
+</section>
+
+<section className="elements">
+
+</section>
+
+<footer className="footer">© 2025 Around The U.S.</footer>
+
     </>
   )
 }
