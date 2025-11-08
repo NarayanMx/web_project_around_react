3d4b475bb80176eae370e5c20165a19ebddc6f59
Narayan Bt <narayan.pre@gmail.com>
2025-11-07 18:23:55 -0600

    Creación de modulo reutilizable -Popup-

--- src/components/Main/Popup/Popup.jsx
+++ src/components/Main/Popup/Popup.jsx
@@ -1,7 +1,33 @@
 export default function Popup(props) {
 
-  const {title, children} = props;
+  const {title, children, onClose} = props;
+
+  const handleOverlayClick =(e) => {
+    if (e.target === e.currentTarget) {
+      onClose();
+    }
+  };
+
   return (
-    <div>Popup</div>
+    <div className="popup" onClick={handleOverlayClick}> 
+        <div className="popup__container">
+
+          <button className="popup__close-button" onClick={onClose}>
+            <img
+              src="./images/Close_Icon.png"
+              alt="botón para cerrar popup"
+              className="popup__close-image"
+            />
+          </button>
+
+          <p className="popup__title">{title}</p>
+
+          <div className="popup__limit">
+
+            {children}
+
+          </div>
+        </div>
+      </div>
   )
 }
