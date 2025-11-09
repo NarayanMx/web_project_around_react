e71394ff684e31c06cb24b7585eb11239410e96c
Narayan Bt <narayan.pre@gmail.com>
2025-11-09 16:19:45 -0600

    Importación de componentes a Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { useState } from "react";
+import Popup from "./Popup/Popup.jsx";
+import NewCard from "./form/NewCard/NewCard.jsx"
+import EditProfile from "./form/EditProfile/EditProfile.jsx"
+import EditAvatar from "./form/EditAvatar/EditAvatar.jsx"
 
 
 function Main() {
+
+    const [Popup, setPopup] = useState(null);
+
+
   return (
 <section className="elements">
 
