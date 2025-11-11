be50c78cb1af3f519a651e5245b89ae489bca8b1
Narayan Bt <narayan.pre@gmail.com>
2025-11-10 19:22:17 -0600

    Open y close Popup mediante handle

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -9,8 +9,16 @@ function Main() {
 
     const [popup, setPopup] = useState(null);
 
+    const newCardPopup = { title: "Nuevo lugar", children: <NewCard /> };
+    const editProfilePopup = { title: "Editar Perfíl", children: <EditProfile /> };
+    const editAvatarPopup = { title: "Editar Avatar", children: <EditAvatar /> };
+
     const handleClosePopup = () => {
     setPopup(null);
+    };
+
+    const handleOpenPopup = (popup) => {
+    setPopup(popup);
     };
 
 
