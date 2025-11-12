fbdedcf6bd4b55d54501eb2c048128530910c62d
Narayan Bt <narayan.pre@gmail.com>
2025-11-11 19:57:54 -0600

    Actualización de importaciones en App

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
 import React from "react";
+import { useState } from "react";
 import Header from "./components/Header/Header";
 import Profile from "./components/Profile/Profile";
 import Main from "./components/Main/Main";
 import Footer from "./components/Footer/Footer";
+import NewCard from "./components/Main/form/NewCard/NewCard.jsx"
+import EditProfile from "./components/Main/form/EditProfile/EditProfile.jsx"
+import EditAvatar from "./components/Main/form/EditAvatar/EditAvatar.jsx"
 
 function App() {
 
