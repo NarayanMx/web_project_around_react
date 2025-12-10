d5b5eef6e27df1ee1b3ce2b5bf2472b910ddfdfe
Narayan Bt <narayan.pre@gmail.com>
2025-12-09 18:26:01 -0600

    Correción de ruta main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,10 @@
-export default function Main() {
-  return null;
-}//
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '../pages/index.css'
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
